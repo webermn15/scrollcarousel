ffc81d86f483e975caa4c39b4347faf2973f3604
Michael Weber <michael.weber@americaneagle.com>
2019-04-26 14:42:41 -0500

    built out gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,14 @@ PLUGINS
 
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
-    gulpSize = require('gulp-size');
+    gulpSize = require('gulp-size'),
+    del = require('del');
 
 var sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cssmin = require('gulp-cssmin');
+
+sass.compiler = require('node-sass');
 
 var concat = require('gulp-concat'),
     uglify = require('gulp-uglify');
@@ -24,23 +27,34 @@ TASKS
 =====
 */
 
-function sass() {
-    return gulp.src('/lib/*.scss')
-    .pipe(plumber({
-        errorHandler: function(err) {
-            console.log('sass task error: ', err);
-            this.emit('end');
-        }
-    }))
-    .pipe(sass())
-    .pipe(autoprefixer({
-        browsers: ['last 6 versions'],
-        cascade: false
-    }))
-    .pipe(cssmin())
-        .pipe(gulp.dest('lib/build'));
+function clean() {
+    return del(['./lib/build']);
 }
 
+function compileSass() {
+    return gulp.src('./lib/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 6 versions'],
+            cascade: false
+        }))
+        .pipe(cssmin())
+        .pipe(gulp.dest('./lib/build'));
+}
+
+function compileJs() {
+    return gulp.src('./lib/*.js')
+        .pipe(plumber())
+        .pipe(concat('scrollcarousel.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./lib/build'));
+}
+
+function watch() {
+    gulp.watch('./lib/*.js', compileJs);
+    gulp.watch('./lib/*.scss', compileSass);
+}
 
 /*
 =====
@@ -48,4 +62,9 @@ EXPORT TASKS
 =====
 */
 
-exports.sass = sass;
+exports.clean = clean;
+exports.css = compileSass;
+exports.js = compileJs;
+exports.watch = watch;
+
+exports.default = gulp.series(clean, gulp.parallel(compileSass, compileJs));
